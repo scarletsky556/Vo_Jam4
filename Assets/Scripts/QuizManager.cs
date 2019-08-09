9f2e3b47b8d7b39a72f28e9eb7e8f0df3c7568dc
Tastuki_Yanase <esanayy@gmail.com>
2019-08-10 04:18:18 +0900

    単語用csv追加。クイズ用のスクリプト追加

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    public TextAsset WordCsv;
+    List<WordGroupe> words = new List<WordGroupe>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
+
+public class WordGroupe
+{
+    public int difficult;
+    public List<Word> wordList;
+}
+
+public class Word
+{
+    public string name;
+    public string systemName;
+}
