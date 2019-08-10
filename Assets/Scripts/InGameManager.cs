5e4fc0565740759224e610fcd4844414ce2bd762
Tastuki_Yanase <esanayy@gmail.com>
2019-08-11 04:16:35 +0900

    問題グループの読み込み処理追加

---
+++ Assets/Scripts/InGameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+
+    public QuizManager quizManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        quizManager.Initialized();
+        quizManager.QuizCreate(3, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,25 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class QuizManager : MonoBehaviour
 {
     public TextAsset WordCsv;
     List<WordGroupe> words = new List<WordGroupe>();
 
-
-    // Start is called before the first frame update
-    void Start()
+    //CSVの読み込み、問題グループの生成
+    public void Initialized()
     {
-        
+        string txt = WordCsv.text;
+        string[] wordlist = txt.Split('\n');
+        for(int i=0;i<wordlist.Length;i++)
+        {
+            words.Add(new WordGroupe());
+            var gtxt = wordlist[i].Split(',');
+            words[i].difficult = int.Parse(gtxt[0]);
+            words[i].wordList = new List<Word>();
+            for (int x=1,j=0;x<gtxt.Length; x++,j++)
+            {
+                words[i].wordList.Add(new Word());
+                words[i].wordList[j].name = gtxt[x];
+                words[i].wordList[j].systemName = gtxt[x + 1];
+                x++;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Word[] QuizCreate(int AnswerNum,int WrongNum,int Type=0)
     {
-        
+        List<WordGroupe> quizList;
+        if(Type==0)
+        {
+            quizList = words;
+        }
+        else
+        {
+            quizList = words.Where(x => x.difficult == Type).ToList();
+        }
+        if (quizList == null || quizList.Count == 0)
+            return null;
+        int q = Random.Range(0, quizList.Count);
+        var quiz = quizList[q].wordList.OrderBy(i => System.Guid.NewGuid()).Take(AnswerNum + WrongNum).ToArray();
+        return quiz;
     }
-
     
 }
 
