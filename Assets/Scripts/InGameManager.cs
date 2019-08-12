0ae4f53fbed6b6e5845329dc3e0e2710148de97d
Tastuki_Yanase <esanayy@gmail.com>
2019-08-12 18:37:01 +0900

    問題生成

--- Assets/Scripts/InGameManager.cs
+++ Assets/Scripts/InGameManager.cs
@@ -4,14 +4,59 @@ using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
-
+    class DiffcultSetting
+    {
+        public int AnswerNum;
+        public int WrongNum;
+        public string[] Character;
+    }
+    List<DiffcultSetting> difficult = new List<DiffcultSetting>();
     public QuizManager quizManager;
     // Start is called before the first frame update
     void Start()
     {
         quizManager.Initialized();
-        quizManager.QuizCreate(3, 2);
+        
+        difficult.Add(new DiffcultSetting
+        {
+            //Easy
+            AnswerNum = 3,
+            WrongNum = 2,
+            Character = new string[]{"yukari","maki","akari"}
+        });
+        difficult.Add(new DiffcultSetting
+        {
+            //Normal
+            AnswerNum = 4,
+            WrongNum = 2,
+            Character = new string[] {"yukari","maki","aoi","akane"}
+        });
+        difficult.Add(new DiffcultSetting
+        {
+            //Hard
+            AnswerNum = 6,
+            WrongNum = 2,
+            Character = new string[] { "yukari", "maki", "aoi", "akane", "akari", "kiritan" }
+        });
+
+        GameStart(0);
     }
+
+    public void GameStart(int diff)
+    {
+        var quiz = quizManager.QuizCreate(difficult[diff].AnswerNum, difficult[diff].WrongNum);
+        for (int i = 0; i < difficult[diff].Character.Length;i++)
+        {
+            Debug.Log(quiz[i].systemName+"_"+difficult[diff].Character[i]);
+        }
+        for(int i= difficult[diff].Character.Length;i<quiz.Length;i++)
+        {
+            Debug.Log("Wrong Answer:"+quiz[i].systemName);
+        }
+        
+    }
+
+    
 
     // Update is called once per frame
     void Update()
