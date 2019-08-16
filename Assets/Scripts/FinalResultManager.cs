571f24efa5e4bd718bf881e00f13e4170d7ea65b
Tastuki_Yanase <esanayy@gmail.com>
2019-08-17 04:03:14 +0900

    ツイート機能追加

--- Assets/Scripts/FinalResultManager.cs
+++ Assets/Scripts/FinalResultManager.cs
@@ -6,9 +6,43 @@ public class FinalResultManager : MonoBehaviour
 {
     public UnityEngine.UI.Text Score;
     public UnityEngine.UI.Text Num;
+    string plusText;
     public void DataSet(int c,int w)
     {
         Num.text = c.ToString();
         Score.text = (c * 10).ToString();
+        if(c>=10)
+        {
+            plusText = "あなたはまさしくボイロ聖徳太子です！";
+        }
+        else if(c>=6)
+        {
+            plusText = "琴葉姉妹が鬼門！";
+        }
+        else if(c>=3)
+        {
+            plusText = "…１人くらいはわかる…？";
+        }
+        else
+        {
+            plusText = "お願いだから１人ずつ喋って！";
+        }
+    }
+    
+    //[SerializeField] string linkUrl = "http://negi-lab.blog.jp/";   // ツイートに挿入するURL
+    [SerializeField] string hashtags = "ボイロ聖徳太子";        // ツイートに挿入するハッシュタグ
+
+    public void ResultTweet()
+    {
+            var url = "https://twitter.com/intent/tweet?"
+                + "text=" + "ボイロ聖徳太子で"+Score.text+"点を取りました！"+plusText
+                //+ "&url=" + linkUrl
+                + "&hashtags=" + hashtags;
+
+#if UNITY_EDITOR
+            Application.OpenURL(url);
+#else
+            Application.OpenURL(url);
+#endif
     }
 }
