c9b2f9fb0c186f58e8f17e45393fd28717ac8600
Tastuki_Yanase <esanayy@gmail.com>
2019-08-18 03:22:00 +0900

    音量調整

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer Master;
+
+    public UnityEngine.UI.Slider MasterSlider;
+    public UnityEngine.UI.Slider BGMSlider;
+
+    public void MaterSliderChange()
+    {
+        Master.SetFloat("Master", MasterSlider.value);
+    }
+
+    public void BGMSliderChange()
+    {
+        Master.SetFloat("BGM", BGMSlider.value);
+    }
+}
